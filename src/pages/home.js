70b71748b90fe8bc997ab8f7759d1ec1e13ae311
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-25 17:10:46 -0300

    [Requisito 3] - Adiciona CSS basico

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Carrinho from './Carrinho';
+import './css/CartPage.css'
 
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h2 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h2>
-        <Carrinho />
+      <div className="cart-page">
+        <div className="search-cart">
+          <input type='text' />
+          <Carrinho />
+        </div>
+        <div className="message-cart">
+          <h2 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h2>
+        </div>
       </div>
     );
   }
