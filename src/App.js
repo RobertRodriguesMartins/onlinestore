de57d331369c3176acd38e9d88b9289e07cb5720
aaguero96 <andre.luiz_1996@hotmail.com>
2021-10-25 21:54:44 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import {
 } from 'react-router-dom';
 import Home from './pages/Home';
 import CartPage from './pages/CartPage';
+import ProductPage from './pages/ProductPage';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
+          <Route
+            path="/product/:category/:id"
+            render={ (props) => <ProductPage { ...props } /> }
+          />
           <Route exact path="/" component={ Home } />
           <Route path="/cart-page" component={ CartPage } />
         </Switch>
---
+++ src/pages/ProductPage.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Carrinho from '../components/Carrinho';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+
+class ProductPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      product: {},
+      showLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getProductById();
+  }
+
+  getProductById = async () => {
+    const { match } = this.props;
+    const { params } = match;
+    this.setState({
+      showLoading: true,
+    });
+    const products = await getProductsFromCategoryAndQuery(params.category, undefined);
+    const product = products.results.find((element) => element.id === params.id);
+    this.setState({
+      product,
+      showLoading: false,
+    });
+  }
+
+  renderProduct = (showLoading, product) => {
+    if (showLoading) {
+      return (
+        <h2>
+          Loading...
+        </h2>
+      );
+    }
+    if (!product) {
+      return (
+        <h2>
+          Produto não encontrado :(
+        </h2>
+      );
+    }
+    return (
+      <div>
+        <img src={ product.thumbnail } alt={ product.title } />
+        <h3 data-testid="product-detail-name">{ product.title }</h3>
+      </div>
+    );
+  }
+
+  render() {
+    const { product, showLoading } = this.state;
+    return (
+      <div>
+        <Carrinho />
+        {
+          this.renderProduct(showLoading, product)
+        }
+      </div>
+    );
+  }
+}
+
+ProductPage.defaultProps = {
+  match: {
+    params: {
+      id: '',
+      category: '',
+    },
+  },
+};
+
+ProductPage.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+      category: PropTypes.string,
+    }),
+  }),
+};
+
+export default ProductPage;
