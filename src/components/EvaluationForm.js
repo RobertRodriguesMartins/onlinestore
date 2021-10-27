2ab108b0f31c7c429e7a31b1bc555ba56af076f4
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-27 11:33:00 -0300

    [Requisito 11] - Adiciona avaliaçao em estrelas

--- src/components/EvaluationForm.js
+++ src/components/EvaluationForm.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
 class EvaluationForm extends React.Component {
+  cumulativeCheckbox = ({ target }) => {
+    target.parentNode.childNodes.forEach((element) => {
+      if (element.value <= target.value) {
+        element.checked = true;
+      } else {
+        element.checked = false;
+      }
+    })
+  }
   render() {
     return (
       <form>
-        <textarea data-testid="product-detail-evaluation" />
+        <input type="email" placeholder="Email" />
+        <div>
+          <input type="checkbox" name="star1" value={ 1 } onChange={this.cumulativeCheckbox} />
+          <input type="checkbox" name="star2" value={ 2 } onChange={this.cumulativeCheckbox} />
+          <input type="checkbox" name="star3" value={ 3 } onChange={this.cumulativeCheckbox} />
+          <input type="checkbox" name="star4" value={ 4 } onChange={this.cumulativeCheckbox} />
+          <input type="checkbox" name="star5" value={ 5 } onChange={this.cumulativeCheckbox} />
+        </div>
+        <label htmlFor="message">
+          <textarea 
+            id="message" 
+            data-testid="product-detail-evaluation"
+            placeholder="Mensagem (opcional)"
+          />
+        </label>
+        <button type="submit">Enviar avaliação</button>
       </form>
     );
   }
